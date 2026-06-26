4dc71309adf5b70efb2de7dd888508c848631ae6
Rostyslav Dabizha <r.dabizhaa@gmail.com>
2026-06-26 22:02:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('R.DABIZHA website');
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+document.querySelectorAll("section, .card, h2").forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(30px)";
+  el.style.transition = "0.8s ease";
+  observer.observe(el);
+});
